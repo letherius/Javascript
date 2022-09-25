e5c76c3c979ddac2bf971a7de395860bee8d7785
letherius Miller <letherius1@gmail.com>
2022-09-25 13:55:48 -0500

    javaScript

---
+++ jsScript.js
@@ -0,0 +1,18 @@
+//console.log("here I am again");
+//var myfirstVar = "Letherius";
+//myfirstVar ="Manuel";
+//var age=36;
+
+const tax = 12;
+let userName = "Saul";
+let age = 36;
+
+
+//console.log("My name is:" + username + " and my age is: "+ age);
+//console.log("the constant is:"+ tax);
+
+document.write("<p> Name :"+ userName +" and the age is:"+age+ "</p>");
+document.write(`
+<p>Name: ${userName}</p>
+<p>age is: ${age}</p>
+`)

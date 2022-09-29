f47ed1b0dcef67e80b6003aef7a8fbc83c1e5154
letherius Miller <letherius1@gmail.com>
2022-09-29 11:05:40 -0500

    assignment 1 js

---
+++ test.js
@@ -0,0 +1,60 @@
+let a = 'Letherius';
+let b = 'break';
+let c = 'continue';
+let d = 'delete';
+let e = 'export';
+let f = 'function';
+let g = 'global';
+let h = 'hypothesis';
+let i = 'integer';
+let j = 'justify';
+let k = 'keyword';
+let l = 'left';
+let m = 'miller';
+let n = 'numerical';
+let o = 'object';
+let p = 'property';
+let q = 'question';
+let r = 'receiver';
+let s = 'string';
+let t = 'type';
+
+document.write(`<h1>${a}</h1>`)
+
+document.write(`<h1>${b}</h1>`)
+
+document.write(`<h1>${c}</h1>`)
+
+document.write(`<h1>${d}</h1>`)
+
+document.write(`<h1>${e}</h1>`)
+
+document.write(`<h1>${f}</h1>`)
+
+document.write(`<h1>${g}</h1>`)
+
+document.write(`<h1>${h}</h1>`)
+
+document.write(`<h1>${i}</h1>`)
+
+document.write(`<h1>${j}</h1>`)
+
+document.write(`<h1>${k}</h1>`)
+
+document.write(`<h1>${l}</h1>`)
+
+document.write(`<h1>${m}</h1>`)
+
+document.write(`<h1>${n}</h1>`)
+
+document.write(`<h1>${o}</h1>`)
+
+document.write(`<h1>${p}</h1>`)
+
+document.write(`<h1>${q}</h1>`)
+
+document.write(`<h1>${r}</h1>`)
+
+document.write(`<h1>${s}</h1>`)
+
+document.write(`<h1>${t}</h1>`)
